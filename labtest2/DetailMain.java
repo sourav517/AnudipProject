3621cb4da8d035ffb79abeec811c9d77dc1cff44
sourav517 <118760143+sourav517@users.noreply.github.com>
2022-12-15 18:20:30 +0530

    Update DetailMain.java

--- labtest2/DetailMain.java
+++ labtest2/DetailMain.java
@@ -2,19 +2,19 @@ package labtest2;
 public class DetailMain {
 	//details of Employees
     private static Employee[] employees = {
-            new Employee("employee1", 30, "9656659895", "Delhi", 3800, "Java developer"),
-            new Employee("employee2", 20, "6487648095", "Mumbai", 4000, "C++ developer"),
-            new Employee("employee4", 50, "8911122223", "Hydrabad", 1500, ""),
-            new Employee("employee5", 60, "7656922295", "Chennai", 100, "Python developer"),
-            new Employee("employee6", 70, "9656016795", "Dheradun", 5500, "Java developer")
+            new Employee("employee1", 30, "9656659895", "Delhi", 38000, "Java developer"),
+            new Employee("employee2", 20, "6487648095", "Mumbai", 40000, "C++ developer"),
+            new Employee("employee4", 50, "8911122223", "Hydrabad", 35000, "Web Developer"),
+            new Employee("employee5", 60, "7656922295", "Chennai", 70000, "Python developer"),
+            new Employee("employee6", 70, "9656016795", "Dheradun", 55000, "Java developer")
     };
     //Details of Managers
     private static Manager[] managers = {
-            new Manager("manager1", 30, "7656065995", "Delhi", 3800, "Java developer"),
-            new Manager("manager2", 20, "8648764895", "Kanpur", 4000, "C++ developer"),
-            new Manager("manager3", 40, "9656159695", "Banglore", 1000, "Manager"),
-            new Manager("manager4", 60, "9656022295", "Kerala", 100, "Python developer"),
-            new Manager("manager5", 70, "8656816795", "Indore", 5500, "Java developer")
+            new Manager("manager1", 30, "7656065995", "Delhi", 73800, "Java developer"),
+            new Manager("manager2", 20, "8648764895", "Kanpur", 84000, "C++ developer"),
+            new Manager("manager3", 40, "9656159695", "Banglore", 671000, "Manager"),
+            new Manager("manager4", 60, "9656022295", "Kerala", 87100, "Python developer"),
+            new Manager("manager5", 70, "8656816795", "Indore", 75500, "Java developer")
     };
 
     
