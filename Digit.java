119f2cf14ce672b4571080b6786d203f29758542
sourav517 <118760143+sourav517@users.noreply.github.com>
2023-08-03 19:26:03 +0530

    Add files via upload

---
+++ Digit.java
@@ -0,0 +1,10 @@
+public class Digit {
+    public static void digit(String[] args)
+    {
+        int a =1;
+        int b =2;
+        int c =3;
+        int s = a+b+c;
+        System.out.println("Sum of three no:"+s);
+    }
+}
